3d70f92fa83a91416da3f5bd31cff495f3b239a1
BRIAN JESUS NUÑEZ CORTES <A25216352@inet.uady.mx>
2026-03-02 08:36:59 -0600

    cambio en el h de vehiculo

--- retos/reto_01/vehiculo.h
+++ retos/reto_01/vehiculo.h
@@ -8,16 +8,20 @@ protected:
     std::string marca;
     std::string modelo;
     int anio;
+    std::string tipo_licencia; /// TIPOS A (AUTOMOVIL), B (MOTO), C (CAMION) D (AUTOBUS)
+    std::string placas; /// PLACA DE VEHICULO
 
 public:
     // Constructores
     Vehiculo();
-    Vehiculo(std::string marca, std::string modelo, int anio);
+    Vehiculo(std::string marca, std::string modelo, int anio, std::string tipo_licencia, std::string placas);
 
     // Métodos para actualizar
     void actualizarMarca(std::string nuevaMarca);
     void actualizarModelo(std::string nuevoModelo);
     void actualizarAnio(int nuevoAnio);
+    void actualizarTipoLicencia(std::string nuevaTipoLicencia);
+    void actualizarPlacas(std::string nuevasPlacas);
 
     // Métodos para mostrar
     void mostrarInformacion();
@@ -26,6 +30,8 @@ public:
     std::string obtenerMarca();
     std::string obtenerModelo();
     int obtenerAnio();
+    std::string obtenerTipoLicencia();
+    std::string obtenerPlacas();
 };
 
 #endif // VEHICULO_H
